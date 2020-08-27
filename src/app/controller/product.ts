14bf6059b67071215eb20e3b4c7774c57faaef86
Joson5 <wangsanmu5@163.com>
2020-08-27 20:02:07 +0800

    add product

---
+++ src/app/controller/product.ts
@@ -0,0 +1,23 @@
+// {
+//   code: 0,
+//   msg: 'ok',
+//   data: [
+//     {
+//       createTime: '2019-08-01T14:54:32',
+//       delStatus: 0,
+//       description: '广西各支队实战平台的雏形版本\n以指挥调度的名义，占实战平台的坑',
+//       icon: null,
+//       id: 36,
+//       layout: 4,
+//       menus:
+//         '[{"menuIcon":"bank","menuId":"32d7ab70-b4c6-11e9-bb8d-b79a87824ae5",         "menuName":"首页","parentMenu":"","menuModule":"","link":"http://note.youdao.com/noteshare?id=143f894bded45feb6a5ef2b54769280d&sub=82FB1EC81DDA4EC9A256AA7A1696039C"}]',
+//       name: '梧州支队实战平台',
+//       plugin:
+//         '[{"id":"1","name":"视频播放插件","version":"2.0.0.40","url":"/files/sfplay_plat_setup.exe"}]',
+//       showLogo: 0,
+//       subTitle: '',
+//       theme: '{"menuTheme":"dark","headTheme":"dark"}',
+//       updateTime: '2019-08-01T14:54:32',
+//     },
+//   ],
+// };
